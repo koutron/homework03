1030a8a0a19d64a507ab9abb7b687697e73588b9
Kouros <kamousavi@gmail.com>
2020-04-03 11:29:07 -0400

    added comments and renamed variable

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 
+// Prompt user for criteria
 var passwordLength = prompt("Please choose a password length between 8 and 128.");
 while(passwordLength < 8 || passwordLength > 128){
   passwordLength = prompt("That number is outside the range.  Please choose a password length between 8 and 128.");
@@ -26,6 +27,7 @@ var upperCaseChoice = confirm("Should the password contain uppercase charecters?
 var numberChoice = confirm("Should the password contain numbers?");
 var specialCharChoice = confirm("Should the password contain special charecters?");
 
+//Criteria validation
 while(!lowerCaseChoice && !upperCaseChoice && !numberChoice && !specialCharChoice){
   alert("You need to select at least one category of charecters.");
   var lowerCaseChoice = confirm("Should the password contain lowercase charecters?");
@@ -34,6 +36,7 @@ while(!lowerCaseChoice && !upperCaseChoice && !numberChoice && !specialCharChoic
   var specialCharChoice = confirm("Should the password contain special charecters?");
 }
 
+//Arrays of all values for each criteria
 var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -41,6 +44,7 @@ var specialCharArr = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
 
 var combinedArr = [];
 
+//For each criteria, add all array values to combinedArr if criteria is true
 if(lowerCaseChoice){
   combinedArr = combinedArr.concat(lowerCaseArr);
 }
@@ -54,12 +58,12 @@ if(specialCharChoice){
   combinedArr = combinedArr.concat(specialCharArr);
 }
 
-var passwordTest = '';
+//Generate password
+var passwordFinal = '';
 for(var i=0; i < passwordLength; i++){
   var randomNum = Math.floor(Math.random() * (combinedArr.length));
-  passwordTest = passwordTest + combinedArr[randomNum];
+  passwordFinal = passwordFinal + combinedArr[randomNum];
 }
-console.log(passwordTest);
-return passwordTest;
+return passwordFinal;
 }
 
